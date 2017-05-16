d1839b0bfb382e71842b55d2c2ed4ee9c00df4f1
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-05-16 20:21:51 +1000

    first commit

---
+++ R/mlr_database.R
@@ -0,0 +1,28 @@
+#' @exportdev
+
+mlr_getPostgres <- function(user, password, host, port, dbname, query){
+  pg = dbDriver("PostgreSQL")
+  con <- dbConnect(pg, user, password, host, dbname, port)
+  query_results <- dbGetQuery(con, query)
+  dbDisconnect(con)
+  query_results
+}
+
+mlr_putPostgres_dataframe <- function(host, port, user, password, db, table, data) {
+  pg = dbDriver("PostgreSQL")
+  message("WARNING: This will drop table before inserting data. Cancel now if this is not acceptable")
+  Sys.sleep(5)
+  con <- dbConnect(pg, user, password, host, db, port)
+  if (dbExistsTable(con, table)) {
+    dbRemoveTable(con, table)
+  }
+  dbWriteTable(con, table, data, row.names = FALSE)
+  message("Data written to Postgres")
+  dbDisconnect(con)
+}
+
+mlr_getMongo <- function (host, db, collection){
+  con <- mongo(collection = collection, db = db, url = host ,verbose = TRUE)
+  mongoData <- con$find()
+  rm(con)
+}
---
+++ R/mlr_visualisations.R
@@ -0,0 +1,41 @@
+mlr_quick_line <- function(x_data, y_data, colour = "#AE93B5", x_label = "x_data", y_label = "y_data"){
+    z <- data.frame(x_data, y_data)
+    ggplot(z, aes(z$x_data, z$y_data)) +
+    geom_line(color = colour, size = 0.8) +
+    geom_point(color = colour, size = 2) +
+    labs(x = x_label) +
+    labs(y = y_label) +
+    mlr_minimal()
+}
+
+mlr_quick_bar <- function(x_data, y_data, colour = "#4A90E2", x_label = "x_data", y_label = "y_data"){
+  z <- data.frame(x_data, y_data)
+  ggplot(z, aes(z$x_data, z$y_data)) +
+    geom_bar(stat = "identity", fill = colour, size = 0.8) +
+    labs(x = x_label) +
+    labs(y = y_label) +
+    mlr_minimal()
+}
+
+
+mlr_basic <- function (base_size = 12, base_family = "") {
+  theme_gray(base_size = base_size, base_family = base_family) %+replace%
+    theme(
+      axis.text = element_text(colour = "#4A4A4A"),
+      axis.title.x = element_text(colour = "#4A4A4A", vjust=0.1),
+      axis.title.y = element_text(colour = "#4A4A4A", angle = 90, vjust=10),
+      axis.ticks = element_line(color = "gray"),
+      panel.background = element_blank(),
+      panel.grid.minor.y = element_line(size=3),
+      panel.grid.major = element_line(colour = "gray"),
+      plot.background = element_rect(fill="white")
+    )
+}
+
+mlr_minimal <-   function(base_size = 12, base_family = ""){
+  theme_bw(base_size = base_size, base_family = base_family) %+replace%
+    theme(axis.ticks = element_blank(), legend.background = element_blank(),
+          legend.key = element_blank(), panel.background = element_blank(),
+          panel.border = element_blank(), strip.background = element_blank(),
+          plot.background = element_blank(), complete = TRUE)
+}
