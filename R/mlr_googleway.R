9e9f3c6f72c97f2653a4ab569ad75d3683878d6b
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-06-05 13:53:42 +1000

    fix

--- R/mlr_googleway.R
+++ R/mlr_googleway.R
@@ -5,13 +5,13 @@ mlr_geocode_address <- function(address, key){
 mlr_google_radar <- function(lat, lng, radius, type, key){
   req <- GET(paste("https://maps.googleapis.com/maps/api/place/radarsearch/json?location=",lat,",",lng,"&radius=",radius,"&type=",type,"&key=",key, sep = ""))
   req_df <- fromJSON(content(req, type = "text", encoding = "UTF-8"))
-  data.frame(cbind(request_json$result$types[1],req_df$results$place_id, req_df$results$geometry$location$lat, req_df$results$geometry$location$lng))
+  data.frame(cbind(req_df$results$place_id, req_df$results$geometry$location$lat, req_df$results$geometry$location$lng))
 }
 
 mlr_google_place <- function(place){
   request <- GET(paste("https://maps.googleapis.com/maps/api/place/details/json?placeid=",place,"&key=",places_key, sep = ""))
   request_json <- fromJSON(content(request, type = "text", encoding = "UTF-8"))
-  place_data <- data.frame(request_json$result$types, request_json$result$id,request_json$result$formatted_address, request_json$result$name, request_json$result$geometry$location$lat, request_json$result$geometry$location$lng)
+  place_data <- data.frame(request_json$result$types[1], request_json$result$id,request_json$result$formatted_address, request_json$result$name, request_json$result$geometry$location$lat, request_json$result$geometry$location$lng)
 }
 
 mlr_googleway <- function(geocode_address, geocode_key, radar_radius, radar_type, places_key){
