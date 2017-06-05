ed517062c436fcf2465f4db16b57814dcd3dde72
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-06-05 13:21:23 +1000

    fixed bug

--- R/mlr_googleway.R
+++ R/mlr_googleway.R
@@ -5,7 +5,7 @@ mlr_geocode_address <- function(address, key){
 mlr_google_radar <- function(lat, lng, radius, type, key){
   req <- GET(paste("https://maps.googleapis.com/maps/api/place/radarsearch/json?location=",lat,",",lng,"&radius=",radius,"&type=",type,"&key=",key, sep = ""))
   req_df <- fromJSON(content(req, type = "text", encoding = "UTF-8"))
-  data.frame(cbind(req_df$results$place_id, req_df$results$geometry$location$lat, req_df$results$geometry$location$lng))
+  data.frame(cbind(request_json$result$types[1],req_df$results$place_id, req_df$results$geometry$location$lat, req_df$results$geometry$location$lng))
 }
 
 mlr_google_place <- function(place){
@@ -16,7 +16,7 @@ mlr_google_place <- function(place){
 
 mlr_googleway <- function(geocode_address, geocode_key, radar_radius, radar_type, places_key){
   req <- mlr_geocode_address(geocode_address, geocode_key)
-  req2 <- mlr_google_radar(request_json$result$types[1], req$results.geometry$location$lat, req$results.geometry$location$lng, radar_radius, radar_type, places_key)
+  req2 <- mlr_google_radar(req$results.geometry$location$lat, req$results.geometry$location$lng, radar_radius, radar_type, places_key)
   places <- as.list(req2$X1)
   data <- lapply(places, FUN = mlr_google_place)
   dataframe <- ldply(data, data.frame)
