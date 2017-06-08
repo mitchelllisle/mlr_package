64a16bd67c0f35ad58e12698d89e6eed2cc1b82c
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-06-08 15:54:22 +1000

    added mlr_s3_putObject

--- R/mlr_s3.R
+++ R/mlr_s3.R
@@ -1,3 +1,10 @@
 mlr_s3_readCSV <- function(s3_file_path){
   read.csv(text = rawToChar(get_object(object = s3_file_path)))
 }
+
+mlr_s3_putObject <- function(object){
+  tmp <- tempfile()
+  on.exit(unlink(tmp))
+  utils::write.csv(spots_data, file = tmp)
+  put_object(tmp, object = object)
+}
