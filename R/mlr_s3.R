82ab08b1305e7dc45b062309d480028656acb6d4
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-06-08 15:56:07 +1000

    Fixed bug with s3_put

--- R/mlr_s3.R
+++ R/mlr_s3.R
@@ -2,9 +2,9 @@ mlr_s3_readCSV <- function(s3_file_path){
   read.csv(text = rawToChar(get_object(object = s3_file_path)))
 }
 
-mlr_s3_putObject <- function(object){
+mlr_s3_putObject <- function(object, file_name){
   tmp <- tempfile()
   on.exit(unlink(tmp))
-  utils::write.csv(spots_data, file = tmp)
-  put_object(tmp, object = object)
-}
+  utils::write.csv(object, file = tmp)
+  put_object(tmp, object = file_name)
+}  
