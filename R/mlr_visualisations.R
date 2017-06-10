fdf9f61a8ddaaef1196617efd1fb62915a244524
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-06-10 14:31:02 +1000

    added dark theme

--- R/mlr_visualisations.R
+++ R/mlr_visualisations.R
@@ -87,3 +87,11 @@ mlr_minimal <-   function(base_size = 11, base_family = ""){
           panel.border = element_blank(), strip.background = element_blank(),
           plot.background = element_blank(), complete = TRUE, plot.title = element_text(size = 16))
 }
+
+mlr_minimal_dark <-   function(base_size = 11, base_family = ""){
+  theme_bw(base_size = base_size, base_family = base_family) %+replace%
+    theme(axis.ticks = element_line("#282828"), legend.background = element_rect("#282828"),
+          legend.key = element_rect("#282828"), panel.background = element_rect("#282828"),
+          panel.border = element_blank(), strip.background = element_rect("#282828"),
+          plot.background = element_rect("#282828"), complete = TRUE, plot.title = element_text(size = 16))
+}
