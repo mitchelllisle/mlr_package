065747dcf78d9b59351080244e7d0e9087a09658
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-07-04 21:45:46 +1000

    added fread() s3 csv function

--- R/mlr_aws.R
+++ R/mlr_aws.R
@@ -2,6 +2,10 @@ mlr_s3_readCSV <- function(s3_file_path){
   read.csv(text = rawToChar(get_object(object = s3_file_path)))
 }
 
+mlr_s3_readCSV_fread <- function(s3_file_path){
+  data.table::fread(rawToChar(get_object(object = s3_file_path)))
+}
+
 mlr_s3_putObject <- function(data, s3_path, col.names = FALSE, row.names = FALSE, quote = FALSE, sep = ",", type = "csv"){
   tmp <- tempfile()
   on.exit(unlink(tmp))
@@ -29,3 +33,39 @@ mlr_getAthena <- function(driver_location, s3_staging, query){
   data <- fetch(query, -1, block = 999)
   return(data)
 }
+
+# .onLoad <- function(libname, pkgname) {
+#   
+#   ## path to the JDBC driver
+#   file <- sprintf('AthenaJDBC41-%s.jar', packageVersion(pkgname))
+#   path <- file.path(system.file('java', package = pkgname), file)
+#   
+#   ## check if the jar is available and install if needed (on first load)
+#   if (!file.exists(path)) {
+#     
+#     url <- paste0('https://s3.amazonaws.com/athena-downloads/drivers/', file)
+#     
+#     ## download the jar file from AWS
+#     try(download.file(url = url, destfile = path, mode = 'wb'),
+#         silent = TRUE)
+#     
+#   }
+#   
+#   ## add the RJDBC driver and the log4j properties file to classpath
+#   rJava::.jpackage(pkgname, lib.loc = libname)
+#   
+# }
+# 
+# .onAttach <- function(libname, pkgname) {
+#   
+#   ## let the user know if the automatic JDBC driver installation failed
+#   path <- system.file('java', package = pkgname)
+#   if (length(list.files(path, '^AthenaJDBC41-[0-9.]*jar$')) == 0) {
+#     packageStartupMessage(
+#       'The automatic installation of the Athena JDBC driver seems to have failed.\n',
+#       'Please check your Internet connection and if the current user can write to ', path, '\n',
+#       'If still having issues, install the jar file manually from:\n',
+#       'https://docs.aws.amazon.com/athena/latest/ug/connect-with-jdbc.html\n')
+#   }
+#   
+# }
