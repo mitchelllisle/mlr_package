ca7b859a3b9d1af69da12e8514b87d840ed20d1e
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-07-04 20:55:32 +1000

    added documentation for mlr_messenger

--- R/mlr_messenger.R
+++ R/mlr_messenger.R
@@ -1,6 +1,25 @@
+#' Simple function for sending messages to Hipchat and Slack dependant on success criteria.
+#' 
+#' @description These functions make it simple to send a message to and from either Slack
+#' using the https://github.com/hrbrmstr/slackr package, or for Hipchat using the 
+#' https://github.com/robertzk/hipchat package. You can set the criteria for whether the 
+#' function should send a Success or Error message to a room of your choosing. No extra
+#' env variables are needed as it works of the existing config for the packages mentioned
+#' above. 
+#'
+#' @param app - Can be either 'Hipchat' or 'Slack'
+#' #' @param channel - the room to send to. No need for # prefix on room name if 
+#' using Slack
+#' @param success_flag - Can be a variable already pre-stored in your script. To use an
+#' existing value, make sure to leave it unquoted when passing to the function
+#' @param success_value - How will the fuction know to send a Success or Error message? Uses
+#' this value and evaluates it against success_flag.
+#' @param success_message - The message to send if success_flag == success_value
+#' @param fail_message - The message to send if success_flag != success_value
+#'
+#' @example mlr_messenger(app = "Hipchat", channel = "AppChat", success_flag = status, success_value = TRUE, success_message = "Success!", fail_message = "Fail!")
 
-
-mlr_messenger <- function(app = "Hipchat", success_flag = "status", channel = "AppChat", success_value = TRUE, success_message = "Success!", fail_message = "Failed!"){
+mlr_messenger <- function(app = "Hipchat", channel = "AppChat", success_flag = status, success_value = TRUE, success_message = "Success!", fail_message = "Failed!"){
   
   # Function for executing Slack message
   slack <- function(){
@@ -17,20 +36,23 @@ mlr_messenger <- function(app = "Hipchat", success_flag = "status", channel = "A
   
   # Function for executing Hipchat message
   hipchat <- function(){
-    if(status == TRUE){
-      hipchat_send('room', 'JARVIS', 'notification',
-                   color = 'green', message = as.character(paste0("@ShielMunroe @FarzanaHaroon @MitchellLisle @AtridgeDCosta So far we've invited ", current_progress$invited,
-                                                                  " and signed up ", current_progress$signed, ". ",
-                                                                  "To hit our target of ", target_results$signupgoal, " we need ",
-                                                                  target_results$invites_per_day, " invites and ",
-                                                                  target_results$signups_per_day, " sign ups per day.")),
-                   notify = TRUE, message_format = 'text')
-      
+    
+    token <- Sys.getenv("HIPCHAT_API_TOKEN")
+    
+    if(token == ""){
+      message("Please set a HIPCHAT_API_TOKEN in Environment variables before sending Hipchat message")
     } else {
-      hipchat_send('room', 'JARVIS', 'notification',
-                   color = 'green', message = as.character('Jarvis sign up goal script failed to run @MitchellLisle'),
-                   notify = TRUE, message_format = 'text')
-      
+      if(success_flag == success_value){
+      hipchat::hipchat_send('room', channel, 'notification',
+                     color = 'green', message = as.character(success_message),
+                     notify = TRUE, message_format = 'text')
+        
+      } else {
+        hipchat::hipchat_send('room', channel, 'notification',
+                     color = 'red', message = as.character(fail_message),
+                     notify = TRUE, message_format = 'text')
+        
+      }
     }
   }
   
