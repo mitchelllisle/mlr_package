5627d03374385753492b4b1db70792b2bcf11e62
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-07-05 21:52:29 +1000

    added mlr_random_jarvis

--- R/mlr_random_jarvis.R
+++ R/mlr_random_jarvis.R
@@ -1,7 +1,7 @@
 #' Generates a random inspirational quote from Jarvis
 mlr_random_jarvis <- function(){
-  index <- round(runif(1, 1, 22), 0)
   quotes <- data.table::fread("https://raw.githubusercontent.com/mitchelllisle/mlr_package/master/R/data/randomjarvis.csv", showProgress = FALSE, header = FALSE)
+  index <- round(runif(1, 1, length(quotes)), 0)
   chosen_quote <- quotes[index]
   as.character(chosen_quote)
 }
