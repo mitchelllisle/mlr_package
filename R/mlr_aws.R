05147d00b89adffb2496a6be14dc7b0a77d6eb5e
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-09-04 17:57:55 +1000

    changed output of function

--- R/mlr_aws.R
+++ R/mlr_aws.R
@@ -1,22 +1,24 @@
 mlr_s3_readCSV <- function(s3_file_path){
-  read.csv(text = rawToChar(get_object(object = s3_file_path)))
+  read.csv(text = rawToChar(aws.s3::get_object(object = s3_file_path)))
 }
 
 mlr_s3_readCSV_fread <- function(s3_file_path){
-  data.table::fread(rawToChar(get_object(object = s3_file_path)))
+  data.table::fread(rawToChar(aws.s3::get_object(object = s3_file_path)))
 }
 
 mlr_s3_putObject <- function(data, s3_path, col.names = FALSE, row.names = FALSE, quote = FALSE, sep = ",", type = "csv"){
   tmp <- tempfile()
   on.exit(unlink(tmp))
   if(type == "csv"){
-    utils::write.table(data, file = tmp, col.names = col.names, row.names = row.names, quote = quote, sep = sep)  
+    utils::write.table(data, file = tmp, col.names = col.names, row.names = row.names, quote = quote, sep = sep)
   }
   if(type == "json"){
     write_json(x = data, path = tmp)
   }
-  put_object(tmp, object = s3_path)
-}  
+  aws.s3::put_object(tmp, object = s3_path)
+
+  return(paste0("Upload Finished. File: ", s3_path))
+}
 
 mlr_getAthena <- function(driver_location, s3_staging, query){
   # Get Athena Driver
@@ -28,36 +30,36 @@ mlr_getAthena <- function(driver_location, s3_staging, query){
                                      s3_staging_dir = s3_staging,
                                      user = Sys.getenv("AWS_ACCESS_KEY_ID"),
                                      password = Sys.getenv("AWS_SECRET_ACCESS_KEY"))
-  
+
   query <- RJDBC::dbSendQuery(con, query)
   data <- fetch(query, -1, block = 999)
   return(data)
 }
 
 # .onLoad <- function(libname, pkgname) {
-#   
+#
 #   ## path to the JDBC driver
 #   file <- sprintf('AthenaJDBC41-%s.jar', packageVersion(pkgname))
 #   path <- file.path(system.file('java', package = pkgname), file)
-#   
+#
 #   ## check if the jar is available and install if needed (on first load)
 #   if (!file.exists(path)) {
-#     
+#
 #     url <- paste0('https://s3.amazonaws.com/athena-downloads/drivers/', file)
-#     
+#
 #     ## download the jar file from AWS
 #     try(download.file(url = url, destfile = path, mode = 'wb'),
 #         silent = TRUE)
-#     
+#
 #   }
-#   
+#
 #   ## add the RJDBC driver and the log4j properties file to classpath
 #   rJava::.jpackage(pkgname, lib.loc = libname)
-#   
+#
 # }
-# 
+#
 # .onAttach <- function(libname, pkgname) {
-#   
+#
 #   ## let the user know if the automatic JDBC driver installation failed
 #   path <- system.file('java', package = pkgname)
 #   if (length(list.files(path, '^AthenaJDBC41-[0-9.]*jar$')) == 0) {
@@ -67,5 +69,5 @@ mlr_getAthena <- function(driver_location, s3_staging, query){
 #       'If still having issues, install the jar file manually from:\n',
 #       'https://docs.aws.amazon.com/athena/latest/ug/connect-with-jdbc.html\n')
 #   }
-#   
+#
 # }
